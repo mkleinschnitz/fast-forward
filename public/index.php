6dbf6fe9db995610b244d0eea9a72ed0629a7c84
michael.kleinschnitz <michael.kleinschnitz@chromeside.de>
2014-09-26 19:39:29 +0200

    referer added

--- public/index.php
+++ public/index.php
@@ -11,6 +11,12 @@ if (file_exists($configPath)) {
     ];
 }
 
+if (isset($_SERVER['HTTP_REFERER'])) {
+    $referer = $_SERVER['HTTP_REFERER'];
+} else {
+    $referer = '';
+}
+
 $path   = str_replace('index.php', '', $_SERVER['SCRIPT_NAME']);
 $action = str_replace($path, '', $_SERVER['REQUEST_URI']);
 
@@ -19,7 +25,7 @@ $pathParts = explode('?', $action);
 if (count($pathParts) === 2) {
     $action = $pathParts[0];
 } else {
-    die('wrong url with to many ?s');
+    $action = '';
 }
 
 $params = http_build_query($_GET);
@@ -38,7 +44,8 @@ $stats = [
     rand(10000, 99999),
     $action,
     $params,
-    $url
+    $url,
+    $referer
 ];
 
 $content = implode(';', $stats) . PHP_EOL;
